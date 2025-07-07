74db2b19edeced84ceea5887ae7a05f2aa634848
Gielzy-Boateng <desmondboateng483@gmail.com>
2025-07-07 11:46:11 +0000

    feat:mobile responsiveness

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -33,24 +33,26 @@ onMounted(() => {
           <input
             placeholder="Email"
             type="email"
-            class="w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
+            class="w-full sm:w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
             v-model="formData.email"
           />
-          <p class="text-red-400 text-sm mt-1 w-4/5" v-if="errors.email">{{ errors.email[0] }}</p>
+          <p class="text-red-400 text-sm mt-1 w-full sm:w-4/5" v-if="errors.email">
+            {{ errors.email[0] }}
+          </p>
         </div>
         <div class="w-full flex flex-col items-center">
           <input
             placeholder="Password"
             type="password"
-            class="w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
+            class="w-full sm:w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
             v-model="formData.password"
           />
-          <p class="text-red-400 text-sm mt-1 w-4/5" v-if="errors.password">
+          <p class="text-red-400 text-sm mt-1 w-full sm:w-4/5" v-if="errors.password">
             {{ errors.password[0] }}
           </p>
         </div>
         <button
-          class="w-4/5 py-3 mt-4 text-2xl bg-red-500 hover:bg-red-600 text-white rounded-xl font-bold shadow transition duration-200"
+          class="w-full sm:w-4/5 py-3 mt-4 text-2xl bg-red-500 hover:bg-red-600 text-white rounded-xl font-bold shadow transition duration-200"
         >
           Login
         </button>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -46,28 +46,32 @@ onMounted(() => {
           <input
             placeholder="Name"
             type="text"
-            class="w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
+            class="w-full sm:w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
             v-model="formData.name"
           />
-          <p class="text-red-400 text-sm mt-1 w-4/5" v-if="errors.name">{{ errors.name[0] }}</p>
+          <p class="text-red-400 text-sm mt-1 w-full sm:w-4/5" v-if="errors.name">
+            {{ errors.name[0] }}
+          </p>
         </div>
         <div class="w-full flex flex-col items-center">
           <input
             placeholder="Email"
             type="email"
-            class="w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
+            class="w-full sm:w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
             v-model="formData.email"
           />
-          <p class="text-red-400 text-sm mt-1 w-4/5" v-if="errors.email">{{ errors.email[0] }}</p>
+          <p class="text-red-400 text-sm mt-1 w-full sm:w-4/5" v-if="errors.email">
+            {{ errors.email[0] }}
+          </p>
         </div>
         <div class="w-full flex flex-col items-center">
           <input
             placeholder="Password"
             type="password"
-            class="w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
+            class="w-full sm:w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
             v-model="formData.password"
           />
-          <p class="text-red-400 text-sm mt-1 w-4/5" v-if="errors.password">
+          <p class="text-red-400 text-sm mt-1 w-full sm:w-4/5" v-if="errors.password">
             {{ errors.password[0] }}
           </p>
         </div>
@@ -75,15 +79,15 @@ onMounted(() => {
           <input
             placeholder="Confirm Password"
             type="password"
-            class="w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
+            class="w-full sm:w-4/5 p-4 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 shadow-sm bg-white text-lg"
             v-model="formData.password_confirmation"
           />
-          <p class="text-red-400 text-sm mt-1 w-4/5" v-if="errors.password_confirmation">
+          <p class="text-red-400 text-sm mt-1 w-full sm:w-4/5" v-if="errors.password_confirmation">
             {{ errors.password_confirmation[0] }}
           </p>
         </div>
         <button
-          class="w-4/5 py-3 mt-4 text-2xl bg-blue-600 hover:bg-blue-700 text-white rounded-xl font-bold shadow transition duration-200"
+          class="w-full sm:w-4/5 py-3 mt-4 text-2xl bg-blue-600 hover:bg-blue-700 text-white rounded-xl font-bold shadow transition duration-200"
         >
           Register as Employee
         </button>
